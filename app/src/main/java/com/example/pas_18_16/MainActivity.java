ef1ef8d6f7af4a8db2923f2a5a67551f10f3e0b4
abyupandega <abyupandega@gmail.com>
2025-05-22 10:10:46 +0700

    feat [GA] navbottom

--- app/src/main/java/com/example/pas_18_16/MainActivity.java
+++ app/src/main/java/com/example/pas_18_16/MainActivity.java
@@ -7,18 +7,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottom_navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottom_navigation = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frame_layout, new Premier_Fragment())
+                .commit();
+
+        bottom_navigation.setOnItemSelectedListener(item -> {
+            Fragment fragment = null;
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_premier) {
+                fragment = new Premier_Fragment();
+            } else if (itemId == R.id.nav_listolahraga) {
+                fragment = new Listolahraga_Fragment();
+            } else if (itemId == R.id.nav_profile) {
+                fragment = new Profile_fragment();
+            }
+
+            if (fragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frame_layout, fragment)
+                        .commit();
+                return true;
+            }
+            return false;
         });
     }
 }
